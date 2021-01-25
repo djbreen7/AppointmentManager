ceb99603628983a595ffc121d0831840e9f4c5ba
Daniel Breen <daninreallife7@gmail.com>
2021-01-24 17:51:04 -0700

    Add Appointment DAO

--- DAO/src/interfaces/AppointmentDao.java
+++ DAO/src/interfaces/AppointmentDao.java
@@ -1,12 +1,12 @@
 package interfaces;
 
-import model.User;
+import model.Appointment;
 
 import java.util.List;
 
 public interface AppointmentDao {
-    public List<User> getAllUsers();
-    public User getUser(String userName);
-    public void updateUser(User user);
-    public void deleteUser(int userId);
+    public List<Appointment> getAllAppointments();
+    public Appointment getAppointment(int appointmentId);
+    public void updateAppointment(Appointment appointment);
+    public void deleteAppointment(int appointmentId);
 }
---
+++ DAO/src/model/Appointment.java
@@ -0,0 +1,118 @@
+package model;
+
+import java.util.Calendar;
+
+public class Appointment extends BaseEntity {
+
+    private int appointmentId;
+    private int customerId;
+    private int userId;
+    private int contactId;
+    private String title;
+    private String description;
+    private String location;
+    private String type;
+    private Calendar start;
+    private Calendar end;
+
+    public Appointment(
+            int appointmentId,
+            int customerId,
+            int userId,
+            int contactId,
+            String title,
+            String description,
+            String location,
+            String type,
+            Calendar start,
+            Calendar end,
+            Calendar createDate,
+            String createdBy,
+            Calendar lastUpdate,
+            String lastUpdatedBy
+            ) {
+        super(createDate, createdBy, lastUpdate, lastUpdatedBy);
+        this.appointmentId = appointmentId;
+        this.customerId = customerId;
+        this.userId = userId;
+        this.contactId = contactId;
+        this.title = title;
+        this.description = description;
+        this.location = location;
+        this.type = type;
+        this.start = start;
+        this.end = end;
+    }
+
+    public int getAppointmentId() {
+        return appointmentId;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getContactId() {
+        return contactId;
+    }
+
+    public void setContactId(int contactId) {
+        this.contactId = contactId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Calendar getStart() {
+        return start;
+    }
+
+    public void setStart(Calendar start) {
+        this.start = start;
+    }
+
+    public Calendar getEnd() {
+        return end;
+    }
+
+    public void setEnd(Calendar end) {
+        this.end = end;
+    }
+}
